194e78e42cdbd6a423ea4b088a2dcf68da2ba546
Kornsrikranet Tanasrithatchai <korn@KornsMacbook.local>
2024-05-12 17:58:23 +0700

    Update All File

--- README.md
+++ README.md
@@ -11,3 +11,33 @@
     * Documents
     * MongoDB
 
+## NoSQL Working
+    * Collection -> Blogs documents
+
+## Mongoose
+* Mongoose us ab ODM library - Object Document Mapping Library
+    * User model -> User.get(), User.findById()
+    * Blog model -> Blog.deleteOne()
+
+## Schemas & Models
+* Schemas defines the structure of a type of data / document
+    - Properties & property types
+
+* User Schema
+    - name (string), required
+    - age (number)
+    - bio (string), required
+
+* Blog Schema
+    - title (string), required
+    - snippet (string), required
+    - body (string), required
+
+* Models allow ys to communicate with database collections
+
+Blog Model [Blog Schema] -> get,save, delete, etc -> Database Blog Collection
+
+## Getting & Saving
+
+## Outputting Documents in Views
+
--- app.js
+++ app.js
@@ -1,26 +1,76 @@
 // using express
 const express = require('express');
-
+// 
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 // express app
 const app = express();
+
+// Connect Database
+const dbURI = 'mongodb+srv://workdebtasset:XSDOsYWC72UTJMI7@nodeninja.chmdtup.mongodb.net/mongoninja?retryWrites=true&w=majority&appName=nodeninja';
+mongoose.connect(dbURI)
+// listen for requests
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
+// app.get('/add-blog', (req,res) => {
+//     const blog = new Blog({
+//         title: 'New Blog',
+//         snippet: 'This is a new blog',
+//         body: 'This is the body of the new blog'
+//     });
+//     blog.save()
+//    .then((result) => res.send(result))
+//    .catch((err) => console.log(err))
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//    .then((result) => res.send(result))
+//    .catch((err) => console.log(err))
+// });
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('66408712e3d08ee89090bc14')
+//    .then((result) => res.send(result))
+//    .catch((err) => console.log(err))
+// });
+
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ]
-    res.render('index', {title: 'Home', blogs });
+    // const blogs = [
+    //     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    // ]
+    // res.render('index', {title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'about'});
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createAt: -1})
+    .then((result) => {
+        res.render('index', {
+            title: 'All Blogs', 
+            blogs:  result
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
 
 app.get('/blogs/create', (req, res) => {
